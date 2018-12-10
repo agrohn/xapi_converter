6c73ad5c067f84438e0ba1eab2b8b64e6ca5527b
Anssi Gröhn <anssi.grohn@karelia.fi>
2018-12-10 15:00:57 +0200

    added extern declaration

--- include/xapi_entry.h
+++ include/xapi_entry.h
@@ -10,6 +10,7 @@ extern const std::map<std::string, std::string> supportedVerbs;
 extern std::map<std::string, std::string> activityTypes;
 extern std::map<std::string, std::string> TaskNameToTaskID;
 extern std::map<std::string, std::string> UserNameToUserID;
+extern std::map<std::string, std::string> UserIDToUserName;
 ////////////////////////////////////////////////////////////////////////////////
 namespace XAPI
 {
