12dc3bb9eea62ff1fb1a326d0e93607d34742615
Anssi Gröhn <anssi.grohn@karelia.fi>
2019-06-10 12:24:12 +0300

    added proper attributions to code, LICENSE.txt, BSD-LICENSE and COPYING

--- src/xapi_converter.cpp
+++ src/xapi_converter.cpp
@@ -1,7 +1,22 @@
 /*
- * Parses moodle logs and constructs xAPI statements out of them, sending them to LRS.
- * \author anssi grohn at karelia dot fi (c) 2018.
- */
+  This file is part of xapi_converter.  Parses moodle logs and constructs 
+  xAPI statements out of them, sending them to LRS.
+
+  Copyright (C) 2018-2019 Anssi Gröhn
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <xapi_converter.h>
 #include <xapi_errors.h>
 #include <xapi_entry.h>
