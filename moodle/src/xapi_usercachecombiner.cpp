2e4f0b176e5f668a536ef342fa09bdc7ffea29b0
Anssi Gröhn <anssi.grohn@karelia.fi>
2020-05-15 15:28:26 +0300

    working solution to merging several user caches

--- moodle/src/xapi_usercachecombiner.cpp
+++ moodle/src/xapi_usercachecombiner.cpp
@@ -5,92 +5,117 @@
 #include <vector>
 #include <set>
 #include <iostream>
+#include <sstream>
 ////////////////////////////////////////////////////////////////////////////////
 using namespace std;
 using json = nlohmann::json;
 ////////////////////////////////////////////////////////////////////////////////
 struct User
 {
-  string Opiskelija;
-  string Email;
-  string UserID;
-  std::set<string> Roles;
+  string name;
+  string email;
+  string userid;
+  std::set<string> roles;
 };
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
-
+  
+  if ( argc < 3 )
+  {
+    cout << "Usage:\n"
+         << argv[0] << " <merged-output-file>.json <usercache-file1>.json [<usercache-file2>.json  ... ] \n";
+    return 0; 
+  }
+  string outfile = argv[1];
+  
   list<string> userCacheFiles;
   std::map<string,User> users;
   // get all files into list
-  for( int i=1;i<argc;i++)
+  for( int i=2;i<argc;i++)
   {
     userCacheFiles.push_back(argv[i]);
   }
 
   for( auto & userFile : userCacheFiles )
   {
+    cerr << "opening file " << userFile << "\n";
     json content;
     ifstream f(userFile);
+    // skip invalid streams
+    if ( !f ) continue;
+    
     f >> content;
     for ( auto & v : content )
     {
       User user;
-      user.Opiskelija = v["_id"]["Opiskelija"];
-      user.Email = v["_id"]["Email"];
-      vector<string> roles = v["_id"]["Roles"];
-      user.Roles.insert(roles.begin(),roles.end());
-      user.UserID = v["_id"]["UserID"];
+      user.name = v["name"];
+      user.email = v["email"];
+      // remove mailto: if it exists
+      if ( user.email.substr(0,7) == string("mailto:") )
+      {
+        user.email.erase(user.email.begin(), user.email.begin()+7);
+      }
+      
+      vector<string> roles = v["roles"];
+      user.roles.insert(roles.begin(),roles.end());
+      json userid=v["id"];
+      if ( userid.is_null() == false )
+        user.userid = userid;
 
       // does entry for this user already exist
-      auto it = users.find(user.Email);
+      auto it = users.find(user.email);
       if ( it == users.end())
       {
-        users[user.Email] = user;
+        users[user.email] = user;
       }
       else // exists, so merge
       {
         User & orig = it->second;
         // set contains always only unique values, so we can merge directly
-        orig.Roles.insert(user.Roles.begin(), user.Roles.end());
+        orig.roles.insert(user.roles.begin(), user.roles.end());
 
         // does name need updating
-        if ( orig.Opiskelija == "-" || orig.Opiskelija.size() == 0 )
+        if ( orig.name == "-" || orig.name.size() == 0 )
         {
-          orig.Opiskelija = user.Opiskelija;
+          orig.name = user.name;
         }
         // update original user id if found
-        if ( orig.UserID.empty() )
+        if ( orig.userid.empty() )
         {
-          orig.UserID = user.UserID;
+          orig.userid = user.userid;
         }
-        else
+        else if ( user.userid.empty() == false )
         {
           // some sanity check
-          if ( orig.UserID != user.UserID )
-            throw runtime_error("Mixed moodle userids for same email!");
+          if ( orig.userid != user.userid )
+          {
+            stringstream ss;
+            ss << "Mixed moodle userids for same email! ";
+            ss << orig.userid << " vs " << user.userid;
+            throw runtime_error(ss.str());
+          }
         }
       }
     }
   }
-
   
-
+  // create resulting json 
   json merged = json::array();
   
   for ( auto & user : users )
   {
     User & u = user.second;
     json userJson = {
-                     { "Opiskelija",  u.Opiskelija },
-                     { "Email", u.Email },
-                     { "Roles", u.Roles },
-                     { "UserID", u.UserID }
+                     { "name",  u.name },
+                     { "email", u.email },
+                     { "roles", u.roles },
+                     { "id", u.userid }
     };
     merged.push_back(userJson);
-    
   }
-  ofstream out("merged-users.json");
+  // write resulting json
+  ofstream out(outfile);
   out << merged.dump();
   out.close();
 
