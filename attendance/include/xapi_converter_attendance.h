ce237547628cad22fd93c5af87dc3c0e03514b4d
Anssi Gröhn <anssi.grohn@karelia.fi>
2021-03-08 21:30:44 +0200

    added attendance application

---
+++ attendance/include/xapi_converter_attendance.h
@@ -0,0 +1,53 @@
+/*
+  This file is part of xapi_converter.  
+  Copyright (C) 2018-2021 Anssi Gröhn
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#pragma once
+#include <iostream>     
+#include <fstream>      
+#include <vector>
+#include <string>
+#include <algorithm>    
+#include <iterator>     
+#include <iomanip>
+#include <chrono>
+#include <xapi_converter.h>
+
+#include <cstdlib>
+#include <cerrno>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Exception.hpp>
+#include <boost/program_options.hpp>
+
+namespace XAPI
+{
+
+  class Attendance : public XAPI::Application
+  {
+
+  public:
+
+    std::string attendanceData;
+    Attendance();
+    virtual ~Attendance();
+    bool ParseCustomArguments() override;
+    void ParseAttendance();
+    bool HasAttendanceData() const;
+
+  };
+}
