68d21dc02339df792444172a65ab3337e38cb988
Anssi Gröhn <anssi.grohn@karelia.fi>
2021-04-16 16:03:06 +0300

    added merge arrays source

---
+++ common/src/json_merge_arrays.cpp
@@ -0,0 +1,45 @@
+/*
+  This file is part of xapi_converter.
+  Copyright (C) 2018-2021 Anssi Gröhn
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+////////////////////////////////////////////////////////////////////////////////
+using namespace std;
+using json = nlohmann::json;
+////////////////////////////////////////////////////////////////////////////////
+int main( int argc, char **argv )
+{
+  json result = json::array();
+  for ( int i=1;i<argc-1;i++)
+  {
+    ifstream file(argv[i]);
+    if ( !file.is_open()) throw runtime_error(std::string("Cannot open file ")+std::string(argv[i]));
+    json obj;
+    file >> obj;
+    for ( auto & e : obj )
+    {
+      result.push_back(e);
+    }
+  }
+  ofstream out(argv[argc-1]);
+  out << result;
+  return 0;
+}
+////////////////////////////////////////////////////////////////////////////////
